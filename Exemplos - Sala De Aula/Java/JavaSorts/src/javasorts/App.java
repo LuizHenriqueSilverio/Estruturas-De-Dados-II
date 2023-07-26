d7c1208c7c702eab374a26141fbf560d5a4d72aa
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-07-26 08:39:42 -0300

    Atualizado - aula 26/07/2023

--- Exemplos - Sala De Aula/Java/JavaSorts/src/javasorts/App.java
+++ Exemplos - Sala De Aula/Java/JavaSorts/src/javasorts/App.java
@@ -5,17 +5,42 @@
  */
 package javasorts;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author luizh
  */
 public class App {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static void printArray(int array[]) {
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " | ");
+        }
+    }
+    
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int tam;
         
+        System.out.println("Tamanho do vetor: ");
+        tam = scanner.nextInt();
+        int[] vetor = new int[tam];
+        
+        System.out.println("Dados vetor: ");
+        for(int i = 0; i < vetor.length; i++) {
+            vetor[i] = scanner.nextInt();
+        }
+        
+        System.out.println("Vetor original: ");
+        printArray(vetor);
+        
+        System.out.println("");
+        BubbleSort.bSort(vetor);
+        System.out.println("Vetor ordenado: ");
+        printArray(vetor);
     }
     
 }
