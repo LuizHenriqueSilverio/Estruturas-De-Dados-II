b434dc31db79f91ae9cd5388f151d1c6f7781c3b
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-08-16 08:16:10 -0300

    Atualizado - aula 16/08/2023

--- Exemplos - Sala De Aula/Java/JavaSorts/src/javasorts/SelectionSort.java
+++ Exemplos - Sala De Aula/Java/JavaSorts/src/javasorts/SelectionSort.java
@@ -5,6 +5,8 @@
  */
 package javasorts;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luizh
@@ -26,6 +28,19 @@ public class SelectionSort {
         int tam = array.length;
         for(int i = 0; i < tam - 1; i++) {
             int k = indexMinArray(array, i);
+            int temp = array[i];
+            array[i] = array[k];
+            array[k] = temp;
+        }
+    }
+    
+    public static void sSortComentado(int array[]) {
+        int tam = array.length;
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < tam - 1; i++) {
+            int k = indexMinArray(array, i);
+            System.out.println("Atual: " + array[i] + "Trocando com menor valor: " + array[k]);
+            scanner.nextLine();
             int temp = array[i];
             array[i] = array[k];
             array[k] = temp;
