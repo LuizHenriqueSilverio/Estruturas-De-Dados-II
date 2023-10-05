e8a8914bbf708273798a3fb0fae200542431eedc
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-10-05 08:22:12 -0300

    Atualizado - aula 05/08/2023

--- Exemplos - Sala De Aula/JavaTrees/src/javatrees/Main.java
+++ Exemplos - Sala De Aula/JavaTrees/src/javatrees/Main.java
@@ -12,11 +12,17 @@ package javatrees;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        tree.add(30);
+        tree.add(new Aluno(30, "Luiz"));
+        tree.add(new Aluno(10, "Athos"));
+        tree.add(new Aluno(25, "Gabriel"));
+        tree.add(new Aluno(40, "Guilherme"));
+        tree.add(new Aluno(50, "Gustavo"));
+        /*tree.add(30);
         tree.add(10);
         tree.add(50);
         tree.add(40);
         tree.add(45);
+        */
         
         tree.preOrder();
         System.out.println("");
