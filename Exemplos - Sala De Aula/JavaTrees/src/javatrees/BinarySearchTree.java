ab28172b01e10384d822aa5d305b4f19a9201eaf
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-10-05 08:00:18 -0300

    Atualizado - aula 05/08/2023

--- Exemplos - Sala De Aula/JavaTrees/src/javatrees/BinarySearchTree.java
+++ Exemplos - Sala De Aula/JavaTrees/src/javatrees/BinarySearchTree.java
@@ -18,6 +18,52 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
     
     private Node<T> insert(Node<T> root, Node<T> newNode) {
-        return newNode;
+        if(root == null) {
+            return newNode;
+        }
+        
+        if(newNode.data.compareTo(root.data) > 0) {
+            root.right = insert(root.right, newNode);
+        }else {
+            root.left = insert(root.left, newNode);
+        }
+        
+        return root; //retorna a raiz atualizada.
+    }
+    
+    public void preOrder() {
+        preOrder(root);
+    }
+    
+    private void preOrder(Node<T> root) {
+        if(root != null) {
+            System.out.print(root.data + ", ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+    
+    public void inOrder() {
+        inOrder(root);
+    }
+    
+    private void inOrder(Node<T> root) {
+        if(root != null){
+            inOrder(root.left);
+            System.out.print(root.data + ", ");
+            inOrder(root.right);
+        }
+    }
+    
+    public void postOrder() {
+        postOrder(root);
+    }
+    
+    private void postOrder(Node<T> root) {
+        if(root != null) {
+            postOrder(root.right);
+            postOrder(root.left);
+            System.out.print(root.data + ", ");
+        }
     }
 }
--- Exemplos - Sala De Aula/JavaTrees/src/javatrees/Main.java
+++ Exemplos - Sala De Aula/JavaTrees/src/javatrees/Main.java
@@ -11,6 +11,17 @@ package javatrees;
  */
 public class Main {
     public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.add(30);
+        tree.add(10);
+        tree.add(50);
+        tree.add(40);
+        tree.add(45);
         
+        tree.preOrder();
+        System.out.println("");
+        tree.inOrder();
+        System.out.println("");
+        tree.postOrder();
     }
 }
